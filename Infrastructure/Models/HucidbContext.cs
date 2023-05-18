98857b8b31d3d273ca041bda642691943cfad80d
DESKTOP-L3Q7GFC\KhanhDuyPC <duynguyen1751@gmail.com>
2023-05-18 22:33:20 +0700

    remove connection string in hucidbcontext

--- Infrastructure/Models/HucidbContext.cs
+++ Infrastructure/Models/HucidbContext.cs
@@ -83,10 +83,6 @@ public partial class HucidbContext : DbContext
 
     public virtual DbSet<VoucherStatus> VoucherStatuses { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost, 1433;Initial Catalog=HUCIDB;User Id=sa;Password=Saigon1751@;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Brand>(entity =>
